40b3e27f2081c2ffc814a70e96ceec2b4d44fc53
balili-melchorjr-developer <balili.melchorjr.developer@gmail.com>
2024-02-07 08:39:28 +0800

    Created react JS Forms

--- src/components/CreateRoomPage.jsx
+++ src/components/CreateRoomPage.jsx
@@ -1,9 +1,79 @@
 import React from 'react'
+import { Box, Typography, Button } from '@mui/material'
+import MyDatePickerField from './forms/MyDatePickerField'
+import MyTextField from './forms/MyTextField'
+import MySelectField from './forms/MySelectField'
+import MyMultilineField from './forms/MyMultilineField'
+import { useForm } from 'react-hook-form'
 
 const CreateRoomPage = () => {
+
+  const defaultValues = {
+    name: '',
+    comments: '',
+    status: '',
+  }
+
+  const {handleSubmit, reset, setValue, control} = useForm({defaultValues:defaultValues})
+
+  const submission = (data) => console.log(data)
+
   return (
     <div>
-        <h2>This is Create Room Page</h2>
+      <form onSubmit={handleSubmit(submission)}>
+      <Box sx={{display: 'flex', width: '100%', backgroundColor:'#00003f', marginBottom: '10px'}}>
+        <Typography sx={{marginLeft: '20px', color: '#ffff'}}>
+          Create Records
+        </Typography>
+      </Box>
+      <Box sx={{display: 'flex', width: '100%', boxShadow: 3, padding:4, flexDirection: 'column'}}>
+        <Box sx={{display: 'flex', justifyContent: 'space-around', marginBottom: '40px'}}>
+          <MyTextField
+            label='Name'
+            name='name'
+            control={control}
+            placeholder='Provide a project name'  
+            width={'30%'}
+          />
+
+          <MyDatePickerField
+             label='Start date'
+             name='start_date'
+             control={control} 
+             width={'30%'}
+          />
+          <MyDatePickerField
+             label='End date'
+             name='end_date'
+             control={control} 
+             width={'30%'}
+          />
+        </Box>
+        <Box sx={{display: 'flex', justifyContent: 'space-around'}}>
+          <MyMultilineField
+              label='Comments'
+              name='comments'
+              control={control}
+              placeholder='Provide a project comments'  
+              width={'30%'}
+            />
+
+            <MySelectField
+              label='Status'
+              name='status'
+              control={control} 
+              width={'30%'}
+            />
+
+            <Box sx={{width:'30%'}}>
+                <Button variant="contained" type="submit" sx={{width: '100%'}}>
+                  Submit
+                </Button>
+            </Box>
+
+        </Box>
+      </Box>
+      </form>
     </div>
   )
 }
---
+++ src/components/forms/MyMultilineField.jsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import TextField from '@mui/material/TextField';
+import { Controller } from 'react-hook-form'
+import { Box, Typography } from '@mui/material'
+
+export default function MyMultilineField(props) {
+    const { label, width, placeholder, name, control } = props
+  return (
+    <Box
+      component="form"
+      sx={{
+        '& > :not(style)': { m: 1, width: '25ch' },
+      }}
+      noValidate
+      autoComplete="off"
+    >
+        <Controller
+            name = {name}
+            control = {control}
+            render = {({
+                field: {onChange, value},
+                fieldState:{error},
+                formState,
+                }) => (
+                    <TextField
+                        sx={{width: {width}}}
+                        id="standard-multiline-static"
+                        label={label}
+                        multiline
+                        onChange={onChange}
+                        value={value}
+                        rows={1}
+                        variant="standard"
+                        placeholder={placeholder}
+                        />
+                )}
+            
+        />
+    </Box>
+  );
+}
